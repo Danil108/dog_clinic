1408e077f654c64ca18156651a9fbe34baa425a7
Oleh Raban (Zemelia) <o.raban@internetdevels.com>
2019-10-14 19:46:29 +0300

    Commit...

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -787,3 +787,13 @@ $settings['entity_update_batch_size'] = 50;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+$databases['default']['default'] = array (
+  'database' => 'default',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
